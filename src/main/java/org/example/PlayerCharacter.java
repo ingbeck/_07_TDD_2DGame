0ddf6b81471210f197a00ec5a11fed13cf45f2b1
ingobecker <mail@ingobecker.de>
2024-01-25 11:24:21 +0100

    add static variable for Player x and y

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -2,12 +2,15 @@ package org.example;
 
 public class PlayerCharacter {
 
+    static int x = 0;
+    static int y = 0;
+
     public static int getX(){
-        return 0;
+        return x;
     }
 
     public static int getY(){
-        return 0;
+        return y;
     }
 
     public static int move(String key){
